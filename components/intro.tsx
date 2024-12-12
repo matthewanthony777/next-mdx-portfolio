80c7541d4d3cc165cc8e8a7e5f288b6f7f73dbcb
matthewanthony777 <matthewbarr777@gmail.com>
2024-12-12 15:14:28 +0000

    updated name

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Matthew Anthony &#39;m Barr.</h1>
+        <h1 className='title no-underline'>Matthew Anthony Barr.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
           I&#39;m an Irish software engineer based in London. I&#39;m
           passionate about learning new technologies and sharing knowledge with
