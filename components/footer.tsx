60db03c4e7116ad0631842439a11a39fe8df9bb0
matthewanthony777 <matthewbarr777@gmail.com>
2024-12-15 13:02:04 +0000

    links

--- components/footer.tsx
+++ components/footer.tsx
@@ -29,7 +29,7 @@ const navigation = [
   // },
   {
     name: 'LinkedIn',
-    href: '#',
+    href: 'https://www.linkedin.com/in/matthew-barr-3018a8198/',
     icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill='currentColor' viewBox='0 0 448 512' {...props}>
         <path
@@ -50,7 +50,7 @@ const navigation = [
   },
   {
     name: 'GitHub',
-    href: '#',
+    href: 'https://github.com/matthewanthony777',
     icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
         <path
